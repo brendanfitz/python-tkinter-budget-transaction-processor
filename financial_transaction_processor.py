2ad466a071fb9e403cfae007fdaecc4fe40a453a
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-09 01:33:19 -0400

    Move functionality from main.py to FinancialTransactionProcessor class

--- financial_transaction_processor.py
+++ financial_transaction_processor.py
@@ -1,8 +1,58 @@
 
-
-
+from itertools import count
+import pandas as pd
+import tkinter as tk
 
 class FinancialTranascationProcessor(object):
 
     def __init__(self):
-        pass
+        self.window = tk.Tk()
+        self.window.bind('<Escape>', lambda event: self.window.destroy())
+        self.current_row_gen = lambda c=count(): next(c)
+        self.categories = self.read_categories()
+        self.transaction_data = self.read_data('sample_data.csv')
+
+    @staticmethod
+    def read_categories():
+        return (pd.read_csv('categories.csv')
+            .Category
+            .tolist()
+        )
+    
+    @staticmethod
+    def read_data(filename):
+        df = pd.read_csv(filename)
+        data = df.to_dict(orient='records')
+        return data
+    
+    def create_title_bar(self):
+        title_bar = tk.Label(
+            text="Financial Transaction Processor",
+            fg="white",
+            bg="black",
+        )
+        title_bar.grid(row=self.current_row_gen(), column=0)
+    
+    def create_table(self):
+
+        table = tk.Frame()
+        table.grid(row=self.current_row_gen(), column=0)
+        
+        columns = ['Transaction Date', 'Description', 'Amount']
+        for row_num, transaction in enumerate(self.transaction_data):
+            for column_num, column in enumerate(columns):
+                label = tk.Label(table, text=transaction[column])
+                label.grid(row=row_num, column=column_num)
+        
+            self.tkvar = tk.StringVar(self.window)
+            dropdown = tk.OptionMenu(table, self.tkvar, *self.categories)
+            dropdown.grid(row=row_num, column=column_num+2)
+            transaction['category_dropdown'] = dropdown 
+    
+
+    def create_submit_button(self):
+        b1 = tk.Button(text="Submit")
+        b1.grid(row=self.current_row_gen(), column=0)
+
+
+
--- main.py
+++ main.py
@@ -1,49 +1,12 @@
 # Python program to create a table 
    
-from tkinter import *
-import pandas as pd
-from itertools import count
-import random
 from financial_transaction_processor import FinancialTranascationProcessor
 
-current_row_gen = lambda c=count(): next(c)
+app = FinancialTranascationProcessor()
 
-ftproc = FinancialTranascationProcessor()
+app.create_title_bar()
+app.create_table()
+app.create_submit_button()
 
-categories = pd.read_csv('categories.csv').Category.tolist()
-
-window = Tk()
-
-title_bar = Label(
-    text="Financial Transaction Processor",
-    fg="white",
-    bg="black",
-)
-
-title_bar.grid(row=current_row_gen(), column=0)
-
-df = pd.read_csv('sample_data.csv')
-data = df.to_dict(orient='records')
-
-table = Frame()
-table.grid(row=current_row_gen(), column=0)
-
-columns = ['Transaction Date', 'Description', 'Amount']
-
-for row_num, transaction in enumerate(data):
-    for column_num, column in enumerate(columns):
-        label = Label(table, text=transaction[column])
-        label.grid(row=row_num, column=column_num)
-
-    tkvar = StringVar(window)
-    dropdown = OptionMenu(table, tkvar, *categories)
-    dropdown.grid(row=row_num, column=column_num+2)
-    transaction['category_dropdown'] = dropdown 
-
-b1 = Button(text="Submit")
-b1.grid(row=current_row_gen(), column=0)
-
-window.bind('<Escape>', lambda event: window.destroy())
-
-window.mainloop()
+app.window.mainloop()
   
