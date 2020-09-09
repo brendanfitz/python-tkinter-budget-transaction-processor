906e6be3b20dc3d0b69e8f83f946471ced6201b6
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-08 21:42:04 -0400

    Add table to main.py

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 # Python program to create a table 
    
-import tkinter as tk
+from tkinter import *
+import pandas as pd
 
-window = tk.Tk()
+window = Tk()
 
-title_bar = tk.Label(
+title_bar = Label(
     text="Financial Transaction Processor",
     fg="white",
     bg="black",
-    height=10
 )
 
 title_bar.grid(row=0, column=0)
 
-  
+df = pd.read_csv('sample_data.csv')
+
+table = Frame()
+table.grid(row=1, column=0)
+
+columns = ['Transaction Date', 'Description', 'Amount']
+
+for row_num, transaction in df.iterrows():
+    for column_num, column in enumerate(columns):
+        label = Label(table, text=transaction[column])
+        label.grid(row=row_num, column=column_num)
+
 window.mainloop()
   
