83befe3c53db192a6f5e08fe3b49ac280fb5dc70
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-08 21:32:02 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Python program to create a table 
+   
+import tkinter as tk
+
+window = tk.Tk()
+
+title_bar = tk.Label(
+    text="Financial Transaction Processor",
+    fg="white",
+    bg="black",
+    height=10
+)
+
+title_bar.grid(row=0, column=0)
+
+  
+window.mainloop()
+  
