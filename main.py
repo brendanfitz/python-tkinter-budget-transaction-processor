362828675d1714847ea3d4b90317054dcea813e9
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-09 01:02:28 -0400

    Add entry elements to table and submit button

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from tkinter import *
 import pandas as pd
 from itertools import count
+import random
 
 current_row_gen = lambda c=count(): next(c)
 
@@ -18,21 +19,32 @@ title_bar = Label(
 title_bar.grid(row=current_row_gen(), column=0)
 
 df = pd.read_csv('sample_data.csv')
+data = df.to_dict(orient='records')
 
 table = Frame()
 table.grid(row=current_row_gen(), column=0)
 
 columns = ['Transaction Date', 'Description', 'Amount']
 
-for row_num, transaction in df.iterrows():
+for row_num, transaction in enumerate(data):
     for column_num, column in enumerate(columns):
         label = Label(table, text=transaction[column])
         label.grid(row=row_num, column=column_num)
-    e1 = Entry(table)
-    e1.grid(row=row_num, column=column_num+1)
+    entry = Entry(table)
+    entry.grid(row=row_num, column=column_num+1)
+    transaction['entry'] = entry
 
-b1 = Button(text="Submit")
+
+def print_random_trans():
+    row = random.choice(data)
+    row['entry'] = row['entry'].get()
+    print(row)
+
+
+b1 = Button(text="Submit", command=print_random_trans)
 b1.grid(row=current_row_gen(), column=0)
+
+window.bind('<Escape>', lambda event: window.destroy())
 
 window.mainloop()
   
