5789f7758e53f98ae2e339ccd37a85a851b7e7a5
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-09 22:05:23 -0400

    Move all backend attributes and methods to backend class

--- backend.py
+++ backend.py
@@ -5,4 +5,34 @@ import pandas as pd
 class Backend(object):
 
     def __init__(self):
-        pass
+        self.read_data('sample_data.csv')
+
+    def read_data(self, filename):
+        df = pd.read_csv(filename)
+        self.columns = df.columns
+        self.transaction_data = df.to_dict(orient='records')
+        self.categories = self.read_categories()
+        self.vendor_df = self.read_vendor_lookup()
+    
+    @staticmethod
+    def read_categories():
+        return (pd.read_csv('categories.csv')
+            .Category
+            .tolist()
+        )
+    
+    def category_lookup(self, vendor):
+        mask = self.vendor_df.Vendor == vendor
+        category_row = self.vendor_df.loc[mask, 'Category']
+        if not category_row.empty:
+            return category_row.values[0]
+
+    @staticmethod
+    def read_vendor_lookup():
+        return (pd.read_csv('vendor_lookup.csv')
+            .sort_values(['Vendor', 'Category'])
+        )
+    
+    def transaction_data_to_df(self):
+        return pd.DataFrame(self.transaction_data, columns=self.columns)
+    
--- financial_transaction_processor.py
+++ financial_transaction_processor.py
@@ -10,49 +10,20 @@ class FinancialTranascationProcessor(object):
         self.window = tk.Tk()
         self.window.bind('<Escape>', lambda event: self.window.destroy())
         self.current_row_gen = lambda c=count(): next(c)
-        self.read_data('sample_data.csv')
-        self.categories = self.read_categories()
-        self.vendor_df = self.read_vendor_lookup()
         self.font_kwargs = dict(font=('Arial', 14))
         self.create_column_widths_dict()
 
-    @staticmethod
-    def read_categories():
-        return (pd.read_csv('categories.csv')
-            .Category
-            .tolist()
-        )
-    
-    def category_lookup(self, vendor):
-        mask = self.vendor_df.Vendor == vendor
-        category_row = self.vendor_df.loc[mask, 'Category']
-        if not category_row.empty:
-            return category_row.values[0]
-
-
-    @staticmethod
-    def read_vendor_lookup():
-        return (pd.read_csv('vendor_lookup.csv')
-            .sort_values(['Vendor', 'Category'])
-        )
-    
-    def read_data(self, filename):
-        df = pd.read_csv(filename)
-        self.columns = df.columns
-        self.transaction_data = df.to_dict(orient='records')
-    
-    def transaction_data_to_df(self):
-        return pd.DataFrame(self.transaction_data, columns=self.columns)
-    
     def create_column_widths_dict(self):
-        df = self.transaction_data_to_df()
-        """
-        computes the max of either:
-            the column name length
-            the length of largest string in the column
-        """
+        df = self.backend.transaction_data_to_df()
+
         def max_of_column_name_or_longest_element(x):
+            """
+            computes the max of either:
+                the column name length
+                the length of largest string in the column
+            """
             return max(len(x.name), x.astype(str).str.len().max())
+
         self.column_widths = df.apply(max_of_column_name_or_longest_element)
     
     def create_title_bar(self):
@@ -66,9 +37,9 @@ class FinancialTranascationProcessor(object):
     
     def calc_column_width(self, column):
         if column == 'Category':
-            return max([len(x) for x in self.categories + ['Category']])
+            return max([len(x) for x in self.backend.categories + ['Category']])
         elif column == 'Vendor':
-            vendors = self.vendor_df.Vendor.unique().tolist()
+            vendors = self.backend.vendor_df.Vendor.unique().tolist()
             return max([len(x) for x in vendors + ['Vendor']])
         return self.column_widths[column]
     
@@ -78,7 +49,7 @@ class FinancialTranascationProcessor(object):
 
         self.create_top_bar(columns)
         
-        for r, transaction in enumerate(self.transaction_data):
+        for r, transaction in enumerate(self.backend.transaction_data):
             for c, column in enumerate(columns):
                 width = self.calc_column_width(column)
                 label = tk.Label(self.table_frame, text=transaction[column],
@@ -111,15 +82,15 @@ class FinancialTranascationProcessor(object):
         
     def create_category_dropdown(self, r, c):
         var = tk.StringVar(self.window)
-        dropdown = tk.OptionMenu(self.table_frame, var, *self.categories)
+        dropdown = tk.OptionMenu(self.table_frame, var, *self.backend.categories)
         dropdown.config(
-            width=max([len(x) for x in self.categories]),
+            width=max([len(x) for x in self.backend.categories]),
             relief='sunken', **self.font_kwargs)
         dropdown.grid(row=r, column=c+2)
         return var, dropdown
     
     def create_vendor_dropdown(self, r, c):
-        vendors = self.vendor_df.Vendor.unique().tolist()
+        vendors = self.backend.vendor_df.Vendor.unique().tolist()
 
         var = tk.StringVar(self.window)
         var.trace('w', self.vendor_change_callback)
@@ -137,7 +108,7 @@ class FinancialTranascationProcessor(object):
         b.grid(row=3, column=0)
     
     def submit(self):
-        for transaction in self.transaction_data:
+        for transaction in self.backend.transaction_data:
             transaction['category'] = transaction['category_var'].get()
             del transaction['category_dropdown']
             del transaction['category_var']
@@ -146,7 +117,7 @@ class FinancialTranascationProcessor(object):
             del transaction['vendor_dropdown']
             del transaction['vendor_var']
         
-        (pd.DataFrame(self.transaction_data)
+        (pd.DataFrame(self.backend.transaction_data)
             .to_csv('sample_data_processed.csv', index=False)
         )
         self.window.destroy()
