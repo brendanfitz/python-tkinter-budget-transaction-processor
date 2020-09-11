860b5b5eae2a2be2eca7e88dcb2c55fa470a9fd3
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-11 11:14:43 -0400

    Add argparse

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import argparse
 from financial_transaction_processor import FinancialTranascationProcessor
 
-def main():
+def main(args):
+    if args.test:
+        print("testing mode")
     app = FinancialTranascationProcessor('sample_data.csv')
     app.mainloop()
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--test',
+        help="test by running with 'data/sample.csv' file",
+        action="store_true",
+    )
+    args = parser.parse_args()
+    return args
+
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    main(args)
   
