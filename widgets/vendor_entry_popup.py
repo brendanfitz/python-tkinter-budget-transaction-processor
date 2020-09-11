c90ddbc6f7e8dfecaa10052a870cc35fe86c0327
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-11 19:23:30 -0400

    Reformat vendor_entry_popup.py

--- widgets/vendor_entry_popup.py
+++ widgets/vendor_entry_popup.py
@@ -1,4 +1,4 @@
-from tkinter import Toplevel, Entry, Button, StringVar, messagebox
+from tkinter import Toplevel, Frame, Label, Entry, Button, StringVar, messagebox
 from widgets.dropdown import DropDown
 
 
@@ -8,13 +8,18 @@ class VendorEntryPopup(Toplevel):
         self.controller = controller
         Toplevel.__init__(self)
         self.title("Add Vendor")
-        self.vendor_entry = Entry(self)
-        self.vendor_entry.grid(row=0, column=0)
+        self.frame = Frame(self)
+        self.frame.grid(row=0, column=0, padx=10, pady=10)
+        Label(self.frame, text="Vendor:").grid(row=0, column=0, padx=(0, 50), pady=(15, 5))
+        self.vendor_entry = Entry(self.frame, width=50+2, **self.controller.font_kwargs)
+        self.vendor_entry.grid(row=0, column=1)
+        Label(self.frame, text="Category:").grid(row=1, column=0, padx=(0, 50), pady=(5, 15))
         self.category_var = StringVar(self.controller)
-        self.category_dropdown = DropDown(self, self.controller, self.category_var, 'Category')
-        self.category_dropdown.grid(row=1, column=0)
-        btn = Button(self, text="Submit", command=self.add_vendor)
-        btn.grid(row=2, column=0)
+        self.category_dropdown = DropDown(self.frame, self.controller, self.category_var, 'Category', width=50)
+        self.category_dropdown.grid(row=1, column=1)
+        btn = Button(self.frame, text="Submit", command=self.add_vendor)
+        btn.grid(row=2, column=0, columnspan=2, padx=10, pady=15)
+        self.vendor_entry.focus_set()
     
     def add_vendor(self):
         vendor = self.vendor_entry.get()
