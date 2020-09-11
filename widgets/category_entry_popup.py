c50ea1619ab49f30e4c3d2c251b6ab3a6ad0b7c1
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-11 17:27:45 -0400

    Stylize category entry popup

--- widgets/category_entry_popup.py
+++ widgets/category_entry_popup.py
@@ -1,4 +1,4 @@
-from tkinter import Toplevel, Entry, Button, StringVar, messagebox
+from tkinter import Toplevel, Frame, Label, Entry, Button, StringVar, messagebox
 from widgets.dropdown import DropDown
 
 
@@ -8,10 +8,14 @@ class CategoryEntryPopup(Toplevel):
         self.controller = controller
         Toplevel.__init__(self)
         self.title("Add Category")
-        self.category_entry = Entry(self)
-        self.category_entry.grid(row=0, column=0)
-        btn = Button(self, text="Submit", command=self.add_category)
-        btn.grid(row=1, column=0)
+        self.frame = Frame(self)
+        self.frame.grid(row=0, column=0, padx=10, pady=10)
+        Label(self.frame, text="Category:").grid(row=0, column=0, padx=(0, 50), pady=15)
+        self.category_entry = Entry(self.frame)
+        self.category_entry.grid(row=0, column=1, pady=25)
+        btn = Button(self.frame, text="Submit", command=self.add_category)
+        btn.grid(row=1, column=0, columnspan=2, padx=10, pady=15)
+        self.category_entry.focus_set()
     
     def add_category(self):
         category = self.category_entry.get()
