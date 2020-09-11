bc6c583c3b0115535596a53acd8c9a623efaa9f8
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-11 02:06:26 -0400

    Stylize FileSelectFrame

--- frames/file_select.py
+++ frames/file_select.py
@@ -8,18 +8,23 @@ class FileSelectFrame(Frame):
         Frame.__init__(self, master)
         self.controller = controller
         self.filepath = None
+        self.padding_kwargs = dict(padx=10, pady=10)
+        self.font_kwargs = dict(font=("Arial", 10))
 
-        title = Label(self, text="File Select Frame")
-        title.grid(row=0, column=0)
+        title = Label(self, text="Bank Transaction Processor", font=("Arial", 18))
+        title.grid(row=0, column=0, **self.padding_kwargs)
 
-        self.select_file_btn = Button(self, text="Select File", command=self.file_dialog)
-        self.select_file_btn.grid(row=1, column=0)
+        self.select_file_btn = Button(self, text="Select File", command=self.file_dialog, **self.font_kwargs)
+        self.select_file_btn.config(height=1, width=15)
+        self.select_file_btn.grid(row=1, column=0, **self.padding_kwargs)
 
-        self.file_label = Label(self, text="")
-        self.file_label.grid(row=1, column=1)
+        self.file_label = Label(self, text="", **self.font_kwargs)
+        self.file_label.config(height=2, width=30)
+        self.file_label.grid(row=2, column=0, **self.padding_kwargs)
 
-        self.submit_btn = Button(self, text="Submit", command=self.submit)
-        self.submit_btn.grid(row=2, column=0)
+        self.submit_btn = Button(self, text="Submit", command=self.submit, **self.font_kwargs)
+        self.submit_btn.config(height=1, width=15)
+        self.submit_btn.grid(row=3, column=0, **self.padding_kwargs)
 
     def file_dialog(self):
         self.filepath = filedialog.askopenfilename(
