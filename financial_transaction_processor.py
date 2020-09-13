9e0588ab8c8b3314ad6456dcdc8772ef51fed605
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-13 18:40:04 -0400

    Create Menubar class
    Moved functionality from financial_transaction_processor module to menu_bar module

--- financial_transaction_processor.py
+++ financial_transaction_processor.py
@@ -2,8 +2,7 @@ from tkinter import Tk, Frame, Menu
 from backend import Backend
 from frames.home import HomeFrame
 from frames.file_select import FileSelectFrame
-from widgets.vendor_entry_popup import VendorEntryPopup
-from widgets.category_entry_popup import CategoryEntryPopup 
+from widgets.menu_bar import MenuBar
 
 class FinancialTranascationProcessor(Tk):
 
@@ -16,17 +15,19 @@ class FinancialTranascationProcessor(Tk):
         self.backend = Backend()
 
         self.container = Frame(self)
-        self.container.pack(side="top", fill="both", expand=True)
+        self.container.pack(fill="both", expand=True)
         self.container.grid_rowconfigure(0, weight=1)
         self.container.grid_columnconfigure(0, weight=1)
 
+        self.menu = MenuBar(self)
+
         self.home_frame = HomeFrame(self.container, self)
-        self.home_frame.grid(row=0, column=0, sticky='nsew')
+        self.home_frame.pack(fill="both", expand=True)
 
         self.file_select_frame = FileSelectFrame(self.container, self)
-        self.file_select_frame.grid(row=0, column=0, sticky='nsew')
+        self.file_select_frame.pack(fill="both", expand=True)
 
-        self.create_menubar()
+        self.menu.add_commands()
 
         self.file_select_frame.tkraise()
 
@@ -40,31 +41,3 @@ class FinancialTranascationProcessor(Tk):
             self.file_select_frame.tkraise()
         else:
             raise ValueError("name must be either 'home' or 'file_select'")
-
-    def create_menubar(self):
-        self.navbar = Frame(
-            self.container,
-            bd=2,
-            relief="groove",
-        )
-        self.navbar.grid(row=0, column=0, columnspan=2, sticky='NSEW')
-
-        self.menu = Menu(self.navbar)
-        self.config(menu=self.menu)
-
-        self.file_menu = Menu(self.menu, tearoff=0)
-
-        self.file_menu.add_command(
-            label="Select File Screen",
-            command=lambda: self.show_frame('file_select')
-        )
-        self.file_menu.add_separator()
-        self.file_menu.add_command(label="Exit", command=lambda: self.destroy())
-
-        self.menu.add_cascade(label="File", menu=self.file_menu)
-
-        self.insert_menu = Menu(self.menu, tearoff=0)
-        self.insert_menu.add_command(label="Category", command=lambda: CategoryEntryPopup(self.home_frame))
-        self.insert_menu.add_command(label="Vendor", command=lambda: VendorEntryPopup(self.home_frame))
-
-        self.menu.add_cascade(label="Insert", menu=self.insert_menu)
